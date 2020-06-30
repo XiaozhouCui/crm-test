a304e5a3a37a995315f05db6bade413d026933e0
Joe Cui <joe.cui@bpanz.com>
2020-06-30 11:24:31 +1000

    Added post api to customers route

--- routes/customers.js
+++ routes/customers.js
@@ -1,13 +1,40 @@
+const Joi = require("@hapi/joi");
 const express = require("express");
 const router = express.Router();
 
+const customers = [
+  { id: 1, name: "customer1" },
+  { id: 2, name: "customer2" },
+  { id: 3, name: "customer3" },
+];
 
-router.get("/api/customers", (req, res) => {
-  res.send([1,2,3]);
+router.get("/", (req, res) => {
+  res.send(customers);
 });
 
-router.get("/api/customers/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:id", (req, res) => {
+  const customer = customers.find((c) => {
+    return c.id === parseInt(req.params.id);
+  });
+  if (!customer) {
+    res.status(404).send("The customer with given ID was not found");
+  }
+  res.send(customer);
+});
+
+router.post("/", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+  });
+  const {error} = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = {
+    id: customers.length + 1,
+    name: req.body.name
+  };
+  customers.push(customer);
+  res.send(customer);
 });
 
 module.exports = router;
