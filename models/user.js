c30490ff42564d8a6f4417afae7a5bd902c95121
Joe Cui <joe.cui@bpanz.com>
2020-08-05 16:06:36 +1000

    Added Client reference in User

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 3,
-    maxlength: 255,
+    maxlength: 256,
     unique: true, // email must be unique
   },
   password: {
@@ -28,13 +28,14 @@ const userSchema = new mongoose.Schema({
     enum: ["admin", "coordinator", "client"],
     default: "client",
   },
+  organisation: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client"
+  },
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign(
-    { _id: this._id, role: this.role },
-    config.get("jwtPrivateKey")
-  );
+  const token = jwt.sign({ _id: this._id, role: this.role }, config.get("jwtPrivateKey"));
   return token;
 };
 
@@ -43,8 +44,9 @@ const User = mongoose.model("User", userSchema);
 function validateUser(user) {
   const schema = Joi.object({
     name: Joi.string().min(3).max(50).required(),
-    email: Joi.string().min(5).max(255).required().email(),
+    email: Joi.string().min(5).max(256).required().email(),
     password: Joi.string().min(5).max(50).required(), // password before hash
+    clientId: Joi.string().max(50),
   });
   return schema.validate(user);
 }
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const auth = require("../middleware/auth");
 const { User, validate } = require("../models/user");
+const { Client } = require("../models/client");
 const _ = require("lodash");
 const bcrypt = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
 
 router.get("/me", auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select("-password"); // exclude the hashed password
+  const user = await User.findById(req.user._id).populate("organisation").select("-password"); // exclude the hashed password
   if (!user) return res.status(404).send("User ID not found.");
   res.send(user);
 });
@@ -19,7 +20,23 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email }); // findOne(), not find by id
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  if (req.body.clientId) {
+    const client = await Client.findById(req.body.clientId);
+    if (!client) return res.status(400).send("Invalid client.");
+    user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      organisation: client,
+    });
+  } else {
+    user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+  }
+
   const salt = await bcrypt.genSalt(10); // salt: 10 rounds by default
   user.password = await bcrypt.hash(user.password, salt); // hash the password
   await user.save();
