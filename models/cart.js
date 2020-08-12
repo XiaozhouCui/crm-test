78bceb9ee15377f0ff3fd8acdc72fa0eb30a0d0c
Joe Cui <joe.cui@bpanz.com>
2020-08-12 14:00:20 +1000

    Updated carts router

--- models/cart.js
+++ models/cart.js
@@ -1,35 +1,35 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const Joi = require("@hapi/joi");
 
-const CartSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    items: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Module",
-      },
-    ],
-    active: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  { timestamps: true }
-);
+// const CartSchema = new mongoose.Schema(
+//   {
+//     user: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//     },
+//     items: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "Module",
+//       },
+//     ],
+//     active: {
+//       type: Boolean,
+//       default: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-const Cart = mongoose.model("Cart", CartSchema);
+// const Cart = mongoose.model("Cart", CartSchema);
 
 function validateCart(req) {
   const schema = Joi.object({
     userId: Joi.objectId().required(),
-    items: Joi.array().items(Joi.objectId()).required(),
+    cart: Joi.array().items(Joi.objectId()).unique().required(),
   });
   return schema.validate(req);
 }
 
-module.exports.Cart = Cart;
+// module.exports.Cart = Cart;
 module.exports.validate = validateCart;
--- routes/carts.js
+++ routes/carts.js
@@ -1,70 +1,19 @@
-const { Cart, validate } = require("../models/cart");
+const { validate } = require("../models/cart");
 const { User } = require("../models/user");
-const { Module } = require("../models/module");
-const auth = require("../middleware/auth");
-const admin = require("../middleware/admin");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const carts = await Cart.find().populate("items user", "name title -_id");
-  res.send(carts);
-});
-
-router.post("/", async (req, res) => {
+router.put('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
-  const user = await User.findById(req.body.userId);
+  
+  const user = await User.findById(req.body.userId).populate("cart");
   if (!user) return res.status(400).send("Invalid user ID.");
+    
+  user.cart = req.body.cart;
 
-  if (req.body.items.length > 0) {
-    const modules = await Module.find().select("_id").lean();
-    const moduleIds = modules.map((item) => String(item._id));
-    for (id of req.body.items) {
-      if (!moduleIds.includes(id)) return res.status(400).send(`Invalid module ID: ${id}`);
-    }
-  }
-
-  let cart = await Cart.findOne({ user: req.body.userId });
-  if (cart) return res.status(400).send("User already has a cart.");
-
-  cart = new Cart({
-    user: user._id,
-    items: req.body.items,
-  });
-  await cart.save();
-  res.send(cart);
-});
-
-router.get("/:id", async (req, res) => {
-  const cart = await Cart.findById(req.params.id);
-  if (!cart) return res.status(404).send("Cart not found.");
-  res.send(cart);
-});
-
-router.put("/:id", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const user = await User.findById(req.body.userId);
-  if (!user) return res.status(400).send("Invalid user ID.");
-
-  const cart = await Cart.findByIdAndUpdate(
-    req.params.id,
-    {
-      items: req.body.items,
-    },
-    { new: true }
-  );
-  if (!cart) return res.status(404).send("Cart not found");
-  res.send(cart);
-});
-
-router.delete("/:id", [auth, admin], async (req, res) => {
-  const cart = await Cart.findByIdAndRemove(req.params.id);
-  if (!cart) return res.status(404).send("The cart with the given ID was not found.");
-  res.send(cart);
+  await user.save();
+  res.send(user);
 });
 
 module.exports = router;
